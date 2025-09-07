097a2696053bfd4b888b7b6342e3e67ffe82d618
Unkn0wn-007 <vadakkemadam2005@gmail.com>
2025-09-07 21:49:22 +0530

    students

---
+++ students.js
@@ -0,0 +1,43 @@
+// This is your new student database.
+// To add a new student, copy a block and fill in the details.
+// Make sure the 'photo' filename matches the image you place in the 'images' folder.
+
+const students = [
+    {
+        name: "Aarav Chen",
+        class: "Beginner's Class",
+        photo: "aarav-chen.jpg"
+    },
+    {
+        name: "Diya Kumar",
+        class: "Intermediate Group",
+        photo: "diya-kumar.jpg"
+    },
+    {
+        name: "Rohan Joshi",
+        class: "Advanced Theory",
+        photo: "rohan-joshi.jpg"
+    },
+    {
+        name: "Zara Ali",
+        class: "Beginner's Class",
+        photo: "zara-ali.jpg"
+    },
+    {
+        name: "Kabir Shah",
+        class: "Advanced Theory",
+        photo: "kabir-shah.jpg"
+    },
+    {
+        name: "Meera Nair",
+        class: "Intermediate Group",
+        photo: "meera-nair.jpg"
+    },
+    // --- ADD YOUR NEXT 34+ STUDENTS HERE ---
+    // Example:
+    // {
+    //     name: "New Student Name",
+    //     class: "Class Name",
+    //     photo: "new-student-photo.jpg"
+    // },
+];
